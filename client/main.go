1ff7115fe478f3e27fcf6a9508fb67fbdfb2da96
binjip978 <binjip978@gmail.com>
2022-07-18 21:20:01 +0300

    extract client login from main and make it standalone

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"flag"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -22,6 +23,68 @@ type cfg struct {
 	file string
 	dest string
 	port int
+}
+
+// Client request file over tftp protocol
+// TODO: add retry params
+type Client struct{}
+
+func (c *Client) Request(addr string, filename string, wr io.Writer) error {
+	conn, err := net.ListenUDP("udp", nil)
+	if err != nil {
+		log.Fatalf("client can't listen on udp socket: %v", err)
+	}
+	defer conn.Close()
+
+	serverAddr, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		log.Fatalf("can't resolve udp address: %v", err)
+	}
+
+	r := packets.RRQ{Filename: filename}
+
+	_, err = conn.WriteToUDP(r.Encode(), serverAddr)
+	if err != nil {
+		log.Fatalf("can't send rrq request: %v", err)
+	}
+
+	buffer := make([]byte, 516)
+
+	for {
+		n, ra, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatalf("can't read from connection: %v", err)
+		}
+		opcode := binary.BigEndian.Uint16(buffer[:2])
+		switch opcode {
+		case 3: // data packet, append data to file
+			d, err := packets.ParseData(buffer[:n])
+			if err != nil {
+				log.Fatalf("can't parse data packet: %v", err)
+			}
+			_, err = wr.Write(d.Data)
+			if err != nil {
+				log.Fatalf("can't write data bytes into file: %v", err)
+			}
+			// send ack to the server
+			ack := &packets.Ack{Block: d.Block}
+			_, err = conn.WriteToUDP(ack.Encode(), ra)
+			if err != nil {
+				log.Fatalf("can't write ack to the server: %v", err)
+			}
+			if len(d.Data) < 512 {
+				return nil
+			}
+		case 5: // report an error to the user
+			e, err := packets.ParseError(buffer[:n])
+			if err != nil {
+				log.Fatalf("can't parse error message: %v", err)
+			}
+			log.Fatal(e.Msg)
+		default:
+			log.Fatalf("unknown opcode from server: %d", opcode)
+		}
+	}
 }
 
 func main() {
@@ -44,66 +107,21 @@ func main() {
 		config.dest = filepath.Join(".", config.file)
 	}
 
-	conn, err := net.ListenUDP("udp", nil)
-	if err != nil {
-		log.Fatalf("client can't listen on udp socket: %v", err)
-	}
-	defer conn.Close()
-
-	serverAddr, err := net.ResolveUDPAddr("udp",
-		net.JoinHostPort(config.addr, strconv.Itoa(config.port)))
-	if err != nil {
-		log.Fatalf("can't resolve udp address: %v", err)
-	}
-
-	r := packets.RRQ{Filename: config.file}
-
-	_, err = conn.WriteToUDP(r.Encode(), serverAddr)
-	if err != nil {
-		log.Fatalf("can't send rrq request: %v", err)
-	}
-
 	f, err := os.OpenFile(config.dest, os.O_CREATE|os.O_EXCL|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("can't create file to save tftp request: %v", err)
 	}
 	defer f.Close()
-	buffer := make([]byte, 516)
 
-	for {
-		n, ra, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			log.Fatalf("can't read from connection: %v", err)
-		}
-		opcode := binary.BigEndian.Uint16(buffer[:2])
-		switch opcode {
-		case 3: // data packet, append data to file
-			d, err := packets.ParseData(buffer[:n])
-			if err != nil {
-				log.Fatalf("can't parse data packet: %v", err)
-			}
-			_, err = f.Write(d.Data)
-			if err != nil {
-				log.Fatalf("can't write data bytes into file: %v", err)
-			}
-			// send ack to the server
-			ack := &packets.Ack{Block: d.Block}
-			_, err = conn.WriteToUDP(ack.Encode(), ra)
-			if err != nil {
-				log.Fatalf("can't write ack to the server: %v", err)
-			}
-			if len(d.Data) < 512 {
-				_ = f.Sync()
-				return
-			}
-		case 5: // report an error to the user
-			e, err := packets.ParseError(buffer[:n])
-			if err != nil {
-				log.Fatalf("can't parse error message: %v", err)
-			}
-			log.Fatal(e.Msg)
-		default:
-			log.Fatalf("unknown opcode from server: %d", opcode)
-		}
+	c := &Client{}
+	addr := net.JoinHostPort(config.addr, strconv.Itoa(config.port))
+	err = c.Request(addr, config.file, f)
+	if err != nil {
+		log.Fatalf("can't request file %s: %v", config.file, err)
+	}
+
+	err = f.Sync()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
