737a950eb3b7a915a4814f8789c2b92dfd3ce2ee
binjip978 <pdp.eleven11@gmail.com>
2022-07-23 21:46:14 +0300

    Add basic server config filepath and port

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/binjip978/tftp/server"
 )
 
 func main() {
+	filePath := flag.String("file", "", "file to serve via tftp (required)")
+	port := flag.Int("port", 6900, "tftp server port")
+	flag.Parse()
+
+	_, err := os.Stat(*filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	srv := server.Server{
-		// Handler: server.BytesHandler([]byte("hello world")),
-		Handler: server.FileHandler("/Users/binjip978/Downloads/tiger.jpeg"),
+		Handler: server.FileHandler(*filePath),
 		Timeout: 2 * time.Second,
 		Retry:   5,
 	}
 
-	log.Fatal(srv.ListenAndServe("127.0.0.1:6900"))
+	log.Fatal(srv.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", *port)))
 }
